99c0f8c955c7a6a7ad021f924614fd0c4e30cdfa
Kreshna Putra Adi Wicaksana <kreshnaputraadi31@student.uns.ac.id>
2022-02-17 07:42:12 +0700

    Update Website

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,22 +48,22 @@ skillsHeader.forEach((el) => {
   el.addEventListener("click", toggleSkills);
 });
 /*==================== QUALIFICATION TABS ====================*/
-const tabs = document.querySelectorAll("[data-target]"),
-  tabContents = document.querySelectorAll("[data-content]");
+const tabs = document.querySelectorAll('[data-target]'),
+  tabContents = document.querySelectorAll('[data-content]');
 
-tabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
     const target = document.querySelector(tab.dataset.target);
 
-    tabContents.forEach((tabContent) => {
-      tabContent.classList.remove("qualification__active");
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('qualification__active');
     });
-    target.classList.add("qualification__active");
+    target.classList.add('qualification__active');
 
-    tab.forEach((tab) => {
-      tab.classList.remove("qualification__active");
+    tab.forEach(tab => {
+      tab.classList.remove('qualification__active');
     });
-    tab.classList.add("qualification__active");
+    tab.classList.add('qualification__active');
   });
 });
 /*==================== SERVICES MODAL ====================*/
